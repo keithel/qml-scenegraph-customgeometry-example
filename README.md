9bfcc5a4ce5ee161f2fc96c1d368adc357a2b154
Keith Kyzivat <keith.kyzivat@qt.io>
2024-12-17 22:41:42 -0500

    Initial import, Qt 5+6 CMakeLists changes
    This imports the example from Qt 6.8.1 examples, and changes the
    CMakeLists.txt file support building for both Qt 5 and 6.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Scene Graph Custom Geometry Example for Qt 6.5+ and Qt 5.15
+
+This provides the Scene Graph Custom Geometry Example as a full CMake project
+that works with both Qt 6 and 5.
+
+## Issues
+
+Right now when you build for Qt 6, Qt Creator doesn't recognize the
+CustomGeometry QML module.
---
+++ main.cpp
@@ -0,0 +1,46 @@
+// Copyright (C) 2016 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+#include "beziercurve.h"
+
+int main(int argc, char *argv[])
+{
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+
+
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    qmlRegisterType<BezierCurve>("CustomGeometry", 1, 0, "BezierCurve");
+
+    const QUrl url(QStringLiteral(
+        "qrc:///scenegraph/customgeometry/Main.qml"
+        ));
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreated,
+        &app,
+        [url](QObject* obj, const QUrl& objUrl) {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        },
+        Qt::QueuedConnection);
+    engine.load(url);
+#else
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+    engine.loadFromModule("CustomGeometry", "Main");
+#endif
+
+    return app.exec();
+}
